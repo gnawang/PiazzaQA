9590b4660768cc56f3b2763d4317442bf5503f29
Takudzwa Rusike <Taku@0587363721.wireless.umich.net>
2015-07-30 16:00:10 -0400

    last couple changes

--- process_data/main.cpp
+++ process_data/main.cpp
@@ -14,18 +14,8 @@ void explore_chidren(xml_node<> *parent){
 
     if( (!strcmp(i->name(), "content") ||
      !strcmp(i->name(), "subject") ) && strlen( i->value() )!=0 ) {
-    //cout << i->name() <<"\n";
     cout << " <TEXT>\n" << i->value() << "</TEXT>\n";
   }
-  //  for (xml_attribute<> *attr = i->first_attribute();
-  //       attr; attr = attr->next_attribute())
-  //  {
-  //      cout << "With attributes " << attr->name() << ":";
-  //      cout << attr->value();
-  //  }
-  //  cout<< "\n";
-
-
   explore_chidren( i );
 
   }
@@ -39,16 +29,5 @@ int main(){
     xml_document<> doc;
     doc.parse<0>(xmlFile.data());
     explore_chidren(&doc);
-
-  //  xml_node<> *node = doc.first_node()->first_node();
-  //  cout << "Name of my first node is: " << node->name() << "\n";
-  //  cout << "Node foobar has value " << node->value() << "\n";
-  //  for (xml_attribute<> *attr = node->first_attribute();
-  //       attr; attr = attr->next_attribute())
-  //  {
-  //      cout << "Node foobar has attribute " << attr->name() << " ";
-  //      cout << "with value " << attr->value() << "\n";
-  //  }
-
   return 0;
 }
